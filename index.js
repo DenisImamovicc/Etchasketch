a33bd0bc51000dce86cf567e36b3e61b74b3863f
Denis Imamovic <Danielarbete@protonmail.com>
2020-04-24 19:08:26 +0200

    Made the x amount of squares adjust their size with the container.What is left of the basic version of the project is a button that refreshes the container to a new canvas and size.

---
+++ index.js
@@ -0,0 +1,66 @@
+const container = document.querySelector('.container');
+
+const square = document.createElement('div');
+square.classList.add('square');
+
+container.appendChild(square);
+
+
+    function requestUserSquareAmount(){ 
+        const Refresh = document.querySelector('.refreshButton');
+            Refresh.addEventListener("click",function() {
+                let userSquareAmount=prompt("How many squares per side you want?" )**2;
+                alert(userSquareAmount);
+
+                massSquareProduction(userSquareAmount);
+
+
+            });
+    
+
+        
+        
+    }
+
+
+
+    function massSquareProduction(userSquareAmount,squareSize) {
+        for (i = 0; i < userSquareAmount; i++) {
+                const container = document.querySelector('.container');
+                const square = document.createElement('div');
+                
+                let squareSize= (256/userSquareAmount)* Math.sqrt(userSquareAmount);
+                square.style.width=squareSize;
+                square.style.height=squareSize;
+
+                square.classList.add('square');
+                container.appendChild(square);                
+                //mathSquareWidth(userSquareAmount);
+
+                square.addEventListener("mouseenter",function(){
+
+                    square.classList.add("squareColored");
+                    square.classList.remove("square");
+
+
+                    
+                });
+
+
+        }
+            
+                
+    }
+
+    function mathSquareWidth(userSquareAmount) {
+        let squareSize= (256/userSquareAmount)* Math.sqrt(userSquareAmount);
+
+        console.log(squareSize);
+        square.style.width=squareSize;
+        square.style.height=squareSize;
+        
+
+
+    }
+
+    requestUserSquareAmount();
