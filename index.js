f94cf83108f0bd5dde404891f47ccf798a6fb599
Denis Imamovic <Danielarbete@protonmail.com>
2020-04-27 20:06:27 +0200

    Added function of random coloring,but it is not implemented properly.Properly would imply being able to press a button to get random colors and another for black color and last one a fading color trail.To switch between the modes one would have to change buttonvariables for now.

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ square.classList.add('square');
 
 container.appendChild(square);
 
+//let blackButton =document.querySelector(".blackButton");
+let blackButton=false;
+//let randomColorButton =document.querySelector(".randomColorButton");
+let randomColorButton=true;
+let gradualBlackButton =document.querySelector(".gradualBlackButton");
+
+
 
 const Refresh = document.querySelector('.refreshButton');
 
@@ -25,7 +32,7 @@ const Refresh = document.querySelector('.refreshButton');
 
 
 
-    function massSquareProduction(userSquareAmount,squareSize) {
+    function massSquareProduction(userSquareAmount) {
         for (i = 0; i < userSquareAmount; i++) {
                 const container = document.querySelector('.container');
                 const square = document.createElement('div');
@@ -35,8 +42,10 @@ const Refresh = document.querySelector('.refreshButton');
                 square.style.height=squareSize;
 
                 square.classList.add('square');
-                container.appendChild(square);                
-
+                container.appendChild(square);     
+                
+                
+            if(blackButton==true){
                 square.addEventListener("mouseenter",function(){
 
                     square.classList.add("squareColored");
@@ -45,6 +54,30 @@ const Refresh = document.querySelector('.refreshButton');
 
                     
                 });
+                console.log("gayyyy");
+            }
+            else if(randomColorButton==true){
+                square.addEventListener("mouseenter",function(){
+
+                    function random_bg_color() {
+                        var x = Math.floor(Math.random() * 256);
+                        var y = Math.floor(Math.random() * 256);
+                        var z = Math.floor(Math.random() * 256);
+                        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+                     console.log(bgColor);
+                      
+                        square.style.background = bgColor;
+                        }
+                    
+                    random_bg_color(); 
+                    
+                    square.classList.add("squareColored");
+                    square.classList.remove("square");
+
+                    
+                });
+                console.log("gayyyyer");
+            }
 
 
         }
@@ -57,8 +90,7 @@ const Refresh = document.querySelector('.refreshButton');
     function mathSquareWidth(userSquareAmount) {
         let squareSize= (256/userSquareAmount)* Math.sqrt(userSquareAmount);
 
-        console.log(squareSize);
-        square.style.width=squareSize;
+      square.style.width=squareSize;
         square.style.height=squareSize;
         
 
