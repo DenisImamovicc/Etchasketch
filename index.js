750144582a8839579faa88caf4cee4398f4dba48
Denis Imamovic <Danielarbete@protonmail.com>
2020-04-29 17:26:08 +0200

    Three buttons with 3 diffrent modes has been added and has the responsivenes for it,except for the 3rd one.Gradient mode,which is not completet in terms of its mathematic formula.One issue that need fixing w the buttons are container clippings that occur when one presses and uses a mode several times to create a canvas and painting them.One can create multiple canvases that stock up on eachother,wich is not the intended function.

--- index.js
+++ index.js
@@ -5,96 +5,148 @@ square.classList.add('square');
 
 container.appendChild(square);
 
-//let blackButton =document.querySelector(".blackButton");
-let blackButton=false;
 //let randomColorButton =document.querySelector(".randomColorButton");
-let randomColorButton=true;
-let gradualBlackButton =document.querySelector(".gradualBlackButton");
+// let randomColorButton=true;
+// let gradualBlackButton =document.querySelector(".gradualBlackButton");
 
 
 
-const Refresh = document.querySelector('.refreshButton');
-
+    const Refresh = document.querySelector('.refreshButton');
     Refresh.addEventListener("click",function() {
         location.reload();
 
 
     });
 
-    function requestUserSquareAmount(){ 
-                let userSquareAmount=prompt("How many squares per side you want?" )**2;
-                alert(userSquareAmount);
-                massSquareProduction(userSquareAmount);
+ 
+
+        let blackButton =document.querySelector(".blackButton");
+        blackButton.addEventListener("click",function() {
+            let userSquareAmount=prompt("How many squares per side you want?" )**2;
+            alert(userSquareAmount);
+            let blackButton=true;
+            let randomColorButton=false;
+            massSquareProduction(userSquareAmount,blackButton,randomColorButton,gradualBlackButton);
+
+
+
+        });
+
+        let randomColorButton =document.querySelector(".randomColorButton");
+        randomColorButton.addEventListener("click",function() {
+            let userSquareAmount=prompt("How many squares per side you want?" )**2;
+            alert(userSquareAmount);
+            let blackButton=false;
+            let randomColorButton=true;
+            massSquareProduction(userSquareAmount,blackButton,randomColorButton,gradualBlackButton);
+
+
+
+        });
+
+        let gradualBlackButton =document.querySelector(".gradualBlackButton");
+        gradualBlackButton.addEventListener("click",function() {
+            let userSquareAmount=prompt("How many squares per side you want?" )**2;
+            alert(userSquareAmount);
+            let blackButton=false;
+            let randomColorButton=false;
+            let gradualBlackButton=true;
+            massSquareProduction(userSquareAmount,blackButton,randomColorButton,gradualBlackButton);
+
+
+
+        });
 
         
         
-    }
+    
 
 
 
-    function massSquareProduction(userSquareAmount) {
+    function massSquareProduction(userSquareAmount,blackButton,randomColorButton,gradualBlackButton) {
         for (i = 0; i < userSquareAmount; i++) {
-                const container = document.querySelector('.container');
-                const square = document.createElement('div');
+                let container = document.querySelector('.container');
+                let square = document.createElement('div');
                 
-                let squareSize= (256/userSquareAmount)* Math.sqrt(userSquareAmount);
-                square.style.width=squareSize;
-                square.style.height=squareSize;
+                trailModeSelector(blackButton,randomColorButton,square,gradualBlackButton);
+                mathSquareWidth(userSquareAmount,square);
 
                 square.classList.add('square');
                 container.appendChild(square);     
                 
-                
-            if(blackButton==true){
-                square.addEventListener("mouseenter",function(){
-
-                    square.classList.add("squareColored");
-                    square.classList.remove("square");
-
-
-                    
-                });
-                console.log("gayyyy");
-            }
-            else if(randomColorButton==true){
-                square.addEventListener("mouseenter",function(){
-
-                    function random_bg_color() {
-                        var x = Math.floor(Math.random() * 256);
-                        var y = Math.floor(Math.random() * 256);
-                        var z = Math.floor(Math.random() * 256);
-                        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-                     console.log(bgColor);
-                      
-                        square.style.background = bgColor;
-                        }
-                    
-                    random_bg_color(); 
-                    
-                    square.classList.add("squareColored");
-                    square.classList.remove("square");
-
-                    
-                });
-                console.log("gayyyyer");
-            }
-
-
         }
             
                 
     }
+    function trailModeSelector(blackButton,randomColorButton,square,gradualBlackButton){
+        if(blackButton==true){
+            square.addEventListener("mouseenter",function(){
+
+                square.classList.add("squareColored");
+                square.classList.remove("square");
 
 
+                
+            });
+            console.log("");
+        }
+        else if(randomColorButton==true){
+            square.addEventListener("mouseenter",function(){
 
-    function mathSquareWidth(userSquareAmount) {
+                function random_bg_color() {
+                    let x = Math.floor(Math.random() * 256);
+                    let y = Math.floor(Math.random() * 256);
+                    let z = Math.floor(Math.random() * 256);
+                    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+                 console.log(bgColor);
+                  
+                    square.style.background = bgColor;
+                    }
+                
+                random_bg_color(); 
+                
+                square.classList.add("squareColored");
+                square.classList.remove("square");
+
+                
+            });
+            console.log("");
+        }
+        else if(gradualBlackButton==true){
+            square.addEventListener("mouseenter",function(){
+               for (i = 9; i > 0; i--) {
+                r_b_gradual_color(); 
+                    
+
+
+                }
+
+                        function r_b_gradual_color() {
+                            let r = 256*(9*0.1);
+                            let g = 256*(9*0.1);
+                            let b = 256*(9*0.1);
+                            let bgColor = "rgb(" + r + "," + g + "," + b + ")";
+                            
+                            console.log(bgColor);
+                            square.style.background = bgColor;
+                            
+                            square.classList.add("squareColored");
+                            square.classList.remove("square");
+                        }
+                    
+            });
+            console.log("");
+        }
+
+    }
+
+
+    function mathSquareWidth(userSquareAmount,square) {
         let squareSize= (256/userSquareAmount)* Math.sqrt(userSquareAmount);
-
-      square.style.width=squareSize;
+        square.style.width=squareSize;
         square.style.height=squareSize;
         
 
 
     }
 
-    requestUserSquareAmount();
